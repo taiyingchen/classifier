95b80628c92ce65821465be2b9dca6477ffedd94
Taiying Chen <taiying.tychen@gmail.com>
2019-12-05 11:12:41 -0600

    Add read csv function

--- classifier/utils.py
+++ classifier/utils.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def read_input(filename):
     X_train = []
     y_train = []
@@ -20,4 +23,27 @@ def read_input(filename):
                 X_train.append(X)
                 y_train.append(label)
 
+    return X_train, y_train, X_test
+
+
+def read_csv(filename):
+    X_train = []
+    y_train = []
+    X_test = []
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                # Ignore header
+                line_count += 1
+            else:
+                # Ignore animal name at row[0]
+                row[1:] = [int(v) for v in row[1:]]
+                if row[-1] == -1:
+                    X_test.append(row[1:-1])
+                else:
+                    X_train.append(row[1:-1])
+                    y_train.append(row[-1])
+                line_count += 1
     return X_train, y_train, X_test
