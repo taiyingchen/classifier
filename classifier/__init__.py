7128cf9c312d425b29b3d7598f943b545197c511
Taiying Chen <taiying.tychen@gmail.com>
2019-12-04 21:30:14 -0600

    Finish decision tree

---
+++ classifier/__init__.py
@@ -0,0 +1,7 @@
+from .tree import DecisionTree
+from .naive_bayes import NaiveBayes
+
+__all__ = [
+    'DecisionTree',
+    'NaiveBayes'
+]
--- classifier/tree.py
+++ classifier/tree.py
@@ -1,38 +1,21 @@
-from sys import stdin
-from utils import read_input
 from math import log2
 from collections import Counter
 
 
-def _read_input():
-    X_train = []
-    y_train = []
-    X_test = []
-    
-    for line in stdin:
-        buffers = line.strip().split()
-        label, attrs = int(buffers[0]), buffers[1:]
-
-        X = []
-        for attr in attrs:
-            name, value = attr.split(':')
-            name, value = int(name), float(value)
-            X.append(value)
-
-        if label == -1:  # Test instance
-            X_test.append(X)
-        else:  # Training instance
-            X_train.append(X)
-            y_train.append(label)
-
-    return X_train, y_train, X_test
+class Node():
+    def __init__(self, depth):
+        self.depth = depth
+        self.attr = None
+        self.split_point = None
+        self.left = None
+        self.right = None
+        self.is_leaf = False
+        self.leaf_class = None
 
 
 class DecisionTree():
     def __init__(self, max_depth):
         self.max_depth = max_depth
-        self.depth = 0
-        self.num_classes = 0
 
     def fit(self, X, y):
         if len(X) == 0 or len(y) == 0:
@@ -43,9 +26,44 @@ class DecisionTree():
         self.num_classes = len(set(y))
         self.num_data = len(X)
         self.num_attrs = len(X[0])
+        self.root = Node(0)
+
+        self.fit_helper(X, y, self.root)
+
+    def fit_helper(self, X, y, node):
+        # Stopping criteria
+        if node.depth >= self.max_depth or len(set(y)) <= 1:
+            node.is_leaf = True
+            node.leaf_class = max(
+                Counter(y).most_common(), key=lambda v: v[1])[0]
+        else:
+            split_attr, split_point = self.find_best_split(X, y)
+            X_left, X_right, y_left, y_right = self.split(
+                X, y, split_attr, split_point)
+            node.attr = split_attr
+            node.split_point = split_point
+            depth = node.depth
+            if len(X_left) > 0:
+                node.left = Node(depth+1)
+                self.fit_helper(X_left, y_left, node.left)
+            if len(X_right) > 0:
+                node.right = Node(depth+1)
+                self.fit_helper(X_right, y_right, node.right)
 
     def predict(self, X):
-        pass
+        y = []
+        for x in X:
+            y.append(self.predict_helper(x))
+        return y
+
+    def predict_helper(self, x):
+        node = self.root
+        while not node.is_leaf:
+            if x[node.attr] <= node.split_point:
+                node = node.left
+            else:
+                node = node.right
+        return node.leaf_class
 
     def info(self, y):
         counter = Counter(y)
@@ -66,37 +84,42 @@ class DecisionTree():
         best_split_point = -1
 
         # Iterate through attributes
-        for i in range(self.num_attrs):
-            attrs = [x[i] for x in X]
+        for attr in range(self.num_attrs):
+            attrs = [x[attr] for x in X]
             # Sort continuous attribute values
             attrs_sorted = sorted(set(attrs))
 
             # Iterate through each split point
             for j in range(len(attrs_sorted)-1):
                 split_point = (attrs_sorted[j] + attrs_sorted[j+1]) / 2
-                left_indices = {index for index in range(len(X)) if X[index][i] <= split_point}
-                # X_left = [X[index] for index in range(len(X)) if index in left_indices]
-                # X_right = [X[index] for index in range(len(X)) if index not in left_indices]
-                y_left = [y[index] for index in range(len(y)) if index in left_indices]
-                y_right = [y[index] for index in range(len(y)) if index not in left_indices]
+                _, _, y_left, y_right = self.split(X, y, attr, split_point)
                 gain = self.info_gain(y, y_left, y_right)
                 if gain > max_info_gain:
                     max_info_gain = gain
                     best_split_point = split_point
-                    split_attr = i
-                print(gain)
-        print(f'Max info gain: {max_info_gain}')
-        print(f'Selected splitting attribute: {split_attr}')
-        print(f'Best split point: {best_split_point}')
+                    split_attr = attr
+        
+        return split_attr, best_split_point
+
+    def split(self, X, y, split_attr, split_point):
+        left_indices = {index for index in range(len(X)) if X[index][split_attr] <= split_point}
+        X_left = [X[index] for index in range(len(X)) if index in left_indices]
+        X_right = [X[index] for index in range(len(X)) if index not in left_indices]
+        y_left = [y[index] for index in range(len(y)) if index in left_indices]
+        y_right = [y[index] for index in range(len(y)) if index not in left_indices]
+        return X_left, X_right, y_left, y_right
+
 
 def main():
-    X_train, y_train, X_test = read_input('input.txt')
-    # print(X_train, y_train, X_test)
+    X_train = [[1.0, 1.0], [1.0, 2.0], [2.0, 1.0], [2.0, 2.0],
+               [3.0, 1.0], [3.0, 2.0], [3.0, 3.0], [4.5, 3.0]]
+    y_train = [1, 1, 1, 3, 1, 3, 3, 3]
+    X_test = [[1.0, 2.2], [4.5, 1.0]]
 
     classifier = DecisionTree(2)
     classifier.fit(X_train, y_train)
-    # print(classifier.info(y_train))
-    classifier.find_best_split(X_train, y_train)
+    y_test = classifier.predict(X_test)
+    print(y_test)
 
 
 if __name__ == '__main__':
